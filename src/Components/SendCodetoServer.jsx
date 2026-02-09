5d09b001f367718f86c9b2d2f7f4c651b824b362
Mohd Anas <mohdanas08022006@gmail.com>
2026-02-09 11:17:15 +0530

    new

--- src/Components/SendCodetoServer.jsx
+++ src/Components/SendCodetoServer.jsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = import.meta.env.VITE_API_BASE;
 export const SendCodeToServer = async (code, input, language) => {
 
     try {
        
-        console.log("API_BASE:", process.env.REACT_APP_API_BASE);
+        console.log("API_BASE:", process.env.VITE_API_BASE);
 
        
         const datafromBackend = await axios.post(`${API_BASE}/api/runcode`, { code, input, language });
