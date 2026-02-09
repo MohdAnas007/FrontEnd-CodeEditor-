333ade30e0c1ce7016226bbae612ffd6facf5bce
Mohd Anas <mohdanas08022006@gmail.com>
2026-02-09 18:32:14 +0530

    anas2

--- src/Components/SendCodetoServer.jsx
+++ src/Components/SendCodetoServer.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
+const API_URL=import.meta.env.VITE_API_URL || 'http://localhost:8000';
+
 export const SendCodeToServer = async (code, input, language) => {
 
     try {
       
-        const datafromBackend = await axios.post(`http://localhost:8080/api/runcode`, { code, input, language });
+        const datafromBackend = await axios.post(`${API_URL}/api/runcode`, { code, input, language });
 
         return datafromBackend.data;
 
